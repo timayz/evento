c3664f74f873f38454781bda35634129ad5d2899
snapiz <jonathan.lapiquonne@gmail.com>
2022-10-17 22:36:13 +0200

    tmp

--- example/src/order/api.rs
+++ example/src/order/api.rs
@@ -1,10 +1,47 @@
-use actix_web::{post, web, Responder, Result, Scope};
+use actix_web::{delete, post, put, web, Responder, Result, Scope};
 
-#[post("place")]
+#[post("/place")]
 async fn place() -> Result<impl Responder> {
     Ok(web::Json(true))
 }
 
+#[post("/add-product")]
+async fn add_product() -> Result<impl Responder> {
+    Ok(web::Json(true))
+}
+
+#[delete("/remove-product")]
+async fn remove_product() -> Result<impl Responder> {
+    Ok(web::Json(true))
+}
+
+#[put("/update-product-quantity")]
+async fn update_product_quantity() -> Result<impl Responder> {
+    Ok(web::Json(true))
+}
+
+#[put("/update-shipping-info")]
+async fn update_shipping_info() -> Result<impl Responder> {
+    Ok(web::Json(true))
+}
+
+#[post("/pay")]
+async fn pay() -> Result<impl Responder> {
+    Ok(web::Json(true))
+}
+
+#[delete("/cancel")]
+async fn cancel() -> Result<impl Responder> {
+    Ok(web::Json(true))
+}
+
 pub fn scope() -> Scope {
-    web::scope("/orders").service(place)
+    web::scope("/orders")
+        .service(place)
+        .service(add_product)
+        .service(remove_product)
+        .service(update_product_quantity)
+        .service(update_shipping_info)
+        .service(pay)
+        .service(cancel)
 }
--- example/src/product/api.rs
+++ example/src/product/api.rs
@@ -1,10 +1,41 @@
-use actix_web::{post, web, Responder, Result, Scope};
+use actix_web::{delete, post, put, web, Responder, Result, Scope};
 
-#[post("")]
+#[post("/create")]
 async fn create() -> Result<impl Responder> {
     Ok(web::Json(true))
 }
 
+#[delete("/delete")]
+async fn delete() -> Result<impl Responder> {
+    Ok(web::Json(true))
+}
+
+#[put("/update-quantity")]
+async fn update_quantity() -> Result<impl Responder> {
+    Ok(web::Json(true))
+}
+
+#[put("/update-visibility")]
+async fn update_visibility() -> Result<impl Responder> {
+    Ok(web::Json(true))
+}
+
+#[put("/update-description")]
+async fn update_description() -> Result<impl Responder> {
+    Ok(web::Json(true))
+}
+
+#[post("/add-review")]
+async fn add_review() -> Result<impl Responder> {
+    Ok(web::Json(true))
+}
+
 pub fn scope() -> Scope {
-    web::scope("/products").service(create)
+    web::scope("/products")
+        .service(create)
+        .service(delete)
+        .service(update_quantity)
+        .service(update_visibility)
+        .service(update_description)
+        .service(add_review)
 }
