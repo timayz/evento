2e69d510b37106fa6f3c7bd717da5719523fb55b
snapiz <jonathan.lapiquonne@gmail.com>
2022-11-29 20:59:13 +0100

    fix: remove sqlx compilation check

--- Makefile
+++ Makefile
@@ -1,6 +1,3 @@
-prepare:
-	SQLX_OFFLINE=true cargo sqlx prepare --database-url postgres://postgres:postgres@localhost:5432/evento_example --merged
-
 create:
 	sqlx database create --database-url postgres://postgres:postgres@localhost:5432/evento_example
 
--- evento/src/event_store.rs
+++ evento/src/event_store.rs
@@ -20,7 +20,7 @@ impl From<sqlx::Error> for Error {
     }
 }
 
-#[derive(Clone, Serialize, Deserialize, Debug)]
+#[derive(Clone, Serialize, Deserialize, Debug, sqlx::FromRow)]
 pub struct Event {
     pub id: Uuid,
     pub name: String,
@@ -240,12 +240,11 @@ impl Engine for PgEngine {
                 query_builder.build().execute(&mut *tx).await?;
             }
 
-            let next_event_id = sqlx::query_as!(
-                Event,
-                r#"SELECT * FROM evento_events WHERE aggregate_id = $1 AND version = $2 LIMIT 1"#,
-                &id,
-                original_version + 1
+            let next_event_id = sqlx::query_as::<_, Event>(
+                "SELECT * FROM evento_events WHERE aggregate_id = $1 AND version = $2 LIMIT 1",
             )
+            .bind(&id)
+            .bind(original_version + 1)
             .fetch_optional(&mut *tx)
             .await?
             .map(|e| e.id)
@@ -271,11 +270,10 @@ impl Engine for PgEngine {
         let pool = self.0.clone();
 
         Box::pin(async move {
-            let events = sqlx::query_as!(
-                Event,
-                r#"SELECT * FROM evento_events WHERE aggregate_id = $1 ORDER BY version"#,
-                &id
+            let events = sqlx::query_as::<_, Event>(
+                "SELECT * FROM evento_events WHERE aggregate_id = $1 ORDER BY version",
             )
+            .bind(&id)
             .fetch_all(&pool)
             .await?;
 
