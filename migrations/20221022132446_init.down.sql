f1c035f62bec37c64f154754278e76cd8dfc42d1
snapiz <jonathan.lapiquonne@gmail.com>
2023-09-29 20:58:15 +0200

    tmp

--- migrations/20221022132446_init.down.sql
+++ migrations/20221022132446_init.down.sql
@@ -1,11 +1,9 @@
 -- Add down migration script here
-
 DROP TABLE IF EXISTS evento_events;
 DROP TABLE IF EXISTS evento_deadletters;
 DROP TABLE IF EXISTS evento_subscriptions;
 
 -- Test only
-
 DO
 $$
 DECLARE
