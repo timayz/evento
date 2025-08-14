db2d5af04f423001e27d4cc604bf0d9c7e2132cb
snapiz <jonathan.lapiquonne@gmail.com>
2025-08-13 20:36:09 -0400

    feat: add support for mysql (#89)

--- evento/tests/sql.rs
+++ evento/tests/sql.rs
@@ -9,10 +9,11 @@ use evento::{
     sql::{Reader, Sql},
     Event,
 };
-use sea_query::{Query, SqliteQueryBuilder};
+use sea_query::{MysqlQueryBuilder, Query, SqliteQueryBuilder};
 use sea_query_binder::SqlxBinder;
 use sqlx::{
-    any::install_default_drivers, migrate::MigrateDatabase, Any, Database, Pool, SqlitePool,
+    any::install_default_drivers, migrate::MigrateDatabase, Any, Database, MySqlPool, Pool,
+    SqlitePool,
 };
 
 use crate::cursor_test::assert_read_result;
@@ -103,8 +104,153 @@ async fn sqlite_subscribe_default_multiple_aggregator() -> anyhow::Result<()> {
 }
 
 #[tokio::test]
-async fn forward_asc() -> anyhow::Result<()> {
+async fn mysql_version() -> anyhow::Result<()> {
+    let executor = create_sqlite_executor("version").await?;
+
+    evento_test::version(&executor).await
+}
+
+#[tokio::test]
+async fn mysql_routing_key() -> anyhow::Result<()> {
+    let executor = create_mysql_executor("routing_key").await?;
+
+    evento_test::routing_key(&executor).await
+}
+
+#[tokio::test]
+async fn mysql_load() -> anyhow::Result<()> {
+    let executor = create_mysql_executor("load").await?;
+
+    evento_test::load(&executor).await
+}
+
+#[tokio::test]
+async fn mysql_invalid_original_version() -> anyhow::Result<()> {
+    let executor = create_mysql_executor("invalid_original_version").await?;
+
+    evento_test::invalid_original_version(&executor).await
+}
+
+#[tokio::test]
+async fn mysql_subscriber_running() -> anyhow::Result<()> {
+    let pool = create_mysql_pool("subscriber_running").await?;
+
+    evento_test::subscriber_running::<Sql<sqlx::MySql>>(&pool.into()).await
+}
+
+#[tokio::test]
+async fn mysql_subscribe() -> anyhow::Result<()> {
+    let pool = create_mysql_pool("subscribe").await?;
+    let data = get_data(&pool).await?;
+
+    evento_test::subscribe::<Sql<sqlx::MySql>>(&pool.into(), data).await
+}
+
+#[tokio::test]
+async fn mysql_subscribe_routing_key() -> anyhow::Result<()> {
+    let pool = create_mysql_pool("subscribe_routing_key").await?;
+    let data = get_data(&pool).await?;
+
+    evento_test::subscribe_routing_key::<Sql<sqlx::MySql>>(&pool.into(), data).await
+}
+
+#[tokio::test]
+async fn mysql_subscribe_default() -> anyhow::Result<()> {
+    let pool = create_mysql_pool("subscribe_default").await?;
+    let data = get_data(&pool).await?;
+
+    evento_test::subscribe_default::<Sql<sqlx::MySql>>(&pool.into(), data).await
+}
+
+#[tokio::test]
+async fn mysql_subscribe_multiple_aggregator() -> anyhow::Result<()> {
+    let pool = create_mysql_pool("subscribe_multiple_aggregator").await?;
+    let data = get_data(&pool).await?;
+
+    evento_test::subscribe_multiple_aggregator::<Sql<sqlx::MySql>>(&pool.into(), data).await
+}
+
+#[tokio::test]
+async fn mysql_subscribe_routing_key_multiple_aggregator() -> anyhow::Result<()> {
+    let pool = create_mysql_pool("subscribe_routing_key_multiple_aggregator").await?;
+    let data = get_data(&pool).await?;
+
+    evento_test::subscribe_routing_key_multiple_aggregator::<Sql<sqlx::MySql>>(&pool.into(), data)
+        .await
+}
+
+#[tokio::test]
+async fn mysql_subscribe_default_multiple_aggregator() -> anyhow::Result<()> {
+    let pool = create_mysql_pool("subscribe_default_multiple_aggregator").await?;
+    let data = get_data(&pool).await?;
+
+    evento_test::subscribe_default_multiple_aggregator::<Sql<sqlx::MySql>>(&pool.into(), data).await
+}
+
+#[tokio::test]
+async fn sqlite_forward_asc() -> anyhow::Result<()> {
     let pool = create_sqlite_pool("forward_asc").await?;
+
+    forward_asc(pool).await
+}
+
+#[tokio::test]
+async fn sqlite_forward_desc() -> anyhow::Result<()> {
+    let pool = create_sqlite_pool("forward_desc").await?;
+
+    forward_desc(pool).await
+}
+
+#[tokio::test]
+async fn sqlite_backward_asc() -> anyhow::Result<()> {
+    let pool = create_sqlite_pool("backward_asc").await?;
+
+    backward_asc(pool).await
+}
+
+#[tokio::test]
+async fn sqlite_backward_desc() -> anyhow::Result<()> {
+    let pool = create_sqlite_pool("backward_desc").await?;
+
+    backward_desc(pool).await
+}
+
+#[tokio::test]
+async fn mysql_forward_asc() -> anyhow::Result<()> {
+    let pool = create_mysql_pool("forward_asc").await?;
+
+    forward_asc(pool).await
+}
+
+#[tokio::test]
+async fn mysql_forward_desc() -> anyhow::Result<()> {
+    let pool = create_mysql_pool("forward_desc").await?;
+
+    forward_desc(pool).await
+}
+
+#[tokio::test]
+async fn mysql_backward_asc() -> anyhow::Result<()> {
+    let pool = create_mysql_pool("backward_asc").await?;
+
+    backward_asc(pool).await
+}
+
+#[tokio::test]
+async fn mysql_backward_desc() -> anyhow::Result<()> {
+    let pool = create_mysql_pool("backward_desc").await?;
+
+    backward_desc(pool).await
+}
+
+async fn forward_asc<DB>(pool: Pool<DB>) -> anyhow::Result<()>
+where
+    DB: Database,
+    for<'c> &'c mut DB::Connection: sqlx::Executor<'c, Database = DB>,
+    sea_query_binder::SqlxValues: for<'q> sqlx::IntoArguments<'q, DB>,
+    usize: sqlx::ColumnIndex<DB::Row>,
+    Event: for<'r> sqlx::FromRow<'r, DB::Row>,
+{
     let data = get_data(&pool).await?;
     let order = Order::Asc;
 
@@ -127,9 +273,14 @@ async fn forward_asc() -> anyhow::Result<()> {
     Ok(())
 }
 
-#[tokio::test]
-async fn forward_desc() -> anyhow::Result<()> {
-    let pool = create_sqlite_pool("forward_desc").await?;
+async fn forward_desc<DB>(pool: Pool<DB>) -> anyhow::Result<()>
+where
+    DB: Database,
+    for<'c> &'c mut DB::Connection: sqlx::Executor<'c, Database = DB>,
+    sea_query_binder::SqlxValues: for<'q> sqlx::IntoArguments<'q, DB>,
+    usize: sqlx::ColumnIndex<DB::Row>,
+    Event: for<'r> sqlx::FromRow<'r, DB::Row>,
+{
     let data = get_data(&pool).await?;
     let order = Order::Desc;
 
@@ -152,9 +303,14 @@ async fn forward_desc() -> anyhow::Result<()> {
     Ok(())
 }
 
-#[tokio::test]
-async fn backward_asc() -> anyhow::Result<()> {
-    let pool = create_sqlite_pool("backward_asc").await?;
+async fn backward_asc<DB>(pool: Pool<DB>) -> anyhow::Result<()>
+where
+    DB: Database,
+    for<'c> &'c mut DB::Connection: sqlx::Executor<'c, Database = DB>,
+    sea_query_binder::SqlxValues: for<'q> sqlx::IntoArguments<'q, DB>,
+    usize: sqlx::ColumnIndex<DB::Row>,
+    Event: for<'r> sqlx::FromRow<'r, DB::Row>,
+{
     let data = get_data(&pool).await?;
     let order = Order::Asc;
 
@@ -177,9 +333,14 @@ async fn backward_asc() -> anyhow::Result<()> {
     Ok(())
 }
 
-#[tokio::test]
-async fn backward_desc() -> anyhow::Result<()> {
-    let pool = create_sqlite_pool("backward_desc").await?;
+async fn backward_desc<DB>(pool: Pool<DB>) -> anyhow::Result<()>
+where
+    DB: Database,
+    for<'c> &'c mut DB::Connection: sqlx::Executor<'c, Database = DB>,
+    sea_query_binder::SqlxValues: for<'q> sqlx::IntoArguments<'q, DB>,
+    usize: sqlx::ColumnIndex<DB::Row>,
+    Event: for<'r> sqlx::FromRow<'r, DB::Row>,
+{
     let data = get_data(&pool).await?;
     let order = Order::Desc;
 
@@ -239,7 +400,7 @@ where
     sea_query_binder::SqlxValues: for<'q> sqlx::IntoArguments<'q, DB>,
 {
     let data = cursor_test::get_data();
-    let mut query = Query::insert()
+    let mut statement = Query::insert()
         .into_table(evento::sql::Event::Table)
         .columns([
             evento::sql::Event::Id,
@@ -255,7 +416,7 @@ where
         .to_owned();
 
     for event in data.clone() {
-        query.values_panic([
+        statement.values_panic([
             event.id.to_string().into(),
             event.name.into(),
             event.data.into(),
@@ -269,8 +430,9 @@ where
     }
 
     let (sql, values) = match DB::NAME {
-        "SQLite" => query.build_sqlx(SqliteQueryBuilder),
-        name => panic!("'{name}' not supported, consider using SQLite"),
+        "SQLite" => statement.build_sqlx(SqliteQueryBuilder),
+        "MySQL" => statement.build_sqlx(MysqlQueryBuilder),
+        name => panic!("'{name}' not supported, consider using SQLite or MySQL"),
     };
 
     sqlx::query_with::<DB, _>(&sql, values)
@@ -280,8 +442,19 @@ where
     Ok(data)
 }
 
+async fn create_mysql_executor(key: impl Into<String>) -> anyhow::Result<Sql<sqlx::MySql>> {
+    Ok(create_mysql_pool(key).await?.into())
+}
+
 async fn create_sqlite_executor(key: impl Into<String>) -> anyhow::Result<Sql<sqlx::Sqlite>> {
     Ok(create_sqlite_pool(key).await?.into())
+}
+
+async fn create_mysql_pool(key: impl Into<String>) -> anyhow::Result<MySqlPool> {
+    let key = key.into();
+    let url = format!("mysql://root:root@localhost:3306/{key}");
+
+    create_pool(url).await
 }
 
 async fn create_sqlite_pool(key: impl Into<String>) -> anyhow::Result<SqlitePool> {
@@ -306,7 +479,9 @@ where
     let pool = Pool::<DB>::connect(&url).await?;
     let schema = Sql::<DB>::get_schema();
 
-    sqlx::query(&schema).execute(&pool).await?;
+    for statement in schema {
+        sqlx::query(&statement).execute(&pool).await?;
+    }
 
     Ok(pool)
 }
