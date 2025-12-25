a294c0187ab75627f89dd46fc9ff88ed94a646d0
jonathan <jonathan.lapiquonne@gmail.com>
2025-12-24 21:24:25 -0400

    asdf

--- examples/bank/src/query/mod.rs
+++ examples/bank/src/query/mod.rs
@@ -3,6 +3,6 @@ pub mod account_details;
 pub mod account_status;
 
 pub use account_details::{
-    load as load_account_details, subscription as account_details_subscription,
-    ACCOUNT_DETAILS_ROWS, AccountDetailsView,
+    ACCOUNT_DETAILS_ROWS, AccountDetailsView, load as load_account_details,
+    subscription as account_details_subscription,
 };
