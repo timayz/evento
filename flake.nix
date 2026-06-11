731c5e3ef584c51f7776e0813c9bc4bf70002acd
jonathan <jonathan.lapiquonne@gmail.com>
2026-06-11 12:56:59 -0400

    chore: flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
   };
