2dc920be57b8d44f3fa0feff723b4c7219c979ca
snapiz <jonathan.lapiquonne@gmail.com>
2026-06-24 23:03:57 -0400

    chore(deps): update all non-major (#207)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- evento-accord/jepsen/project.clj
+++ evento-accord/jepsen/project.clj
@@ -4,9 +4,9 @@
   :license {:name "Apache-2.0"}
   :main evento-accord.core
   :jvm-opts ["-Djava.awt.headless=true"]
-  :dependencies [[org.clojure/clojure "1.11.3"]
+  :dependencies [[org.clojure/clojure "1.12.5"]
                  ;; Jepsen bundles Elle (the strict-serializability checker).
-                 [jepsen "0.3.5"]
-                 [clj-http "3.13.0"]
+                 [jepsen "0.3.11"]
+                 [clj-http "3.13.1"]
                  [cheshire "5.13.0"]]
   :repl-options {:init-ns evento-accord.core})
